6456cef609c43752b3eafa422d543483475daf02
matheus-jacob <math.henrique.jacob@gmail.com>
2023-10-05 14:17:02 -0300

    feat: Add server start on index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 const app = express()
 
+const port = 3001
 app.listen(3001, () => {
-  console.log('Server running on port http://localhost:3001')
+  console.log(`🚀 Server running on port http://localhost:${port}`)
 })
