b899eab1fd23814a7c991380770d6efcea21ab4e
matheus-jacob <math.henrique.jacob@gmail.com>
2023-10-08 14:00:44 -0300

    refactor: Add populate on listOrder

--- api/src/app/useCases/orders/listOrder.ts
+++ api/src/app/useCases/orders/listOrder.ts
@@ -3,7 +3,8 @@ import { Order } from '../../models/Order'
 
 export async function listOrder(req: Request, res: Response) {
   try {
-    const orders = await Order.find()
+    // populate traz todas as informações de determinada entidade ao invés de somente o ID
+    const orders = await Order.find().populate('products.product')
 
     res.json(orders)
   } catch (err) {
