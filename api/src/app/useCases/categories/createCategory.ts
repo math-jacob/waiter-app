e4c3a615eb09aada729c7daee179b57628c2eb0a
matheus-jacob <math.henrique.jacob@gmail.com>
2023-10-08 11:48:53 -0300

    refactor: Add try catch

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -2,9 +2,14 @@ import { Request, Response } from 'express'
 import { Category } from '../../models/Category'
 
 export async function createCategory(req: Request, res: Response) {
-  const { icon, name } = req.body
+  try {
+    const { icon, name } = req.body
 
-  const category = await Category.create({ icon, name })
+    const category = await Category.create({ icon, name })
 
-  res.json(category)
+    res.status(201).json(category)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 }
